983b82f1852fa63d0253d92522b8d3047d9c5871
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-02-21 20:49:12 +0100

    Solved Problem 5

---
+++ src/test/java/engineer/thomas_werner/euler/Problem5Test.java
@@ -0,0 +1,14 @@
+package engineer.thomas_werner.euler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Problem5Test {
+
+    @Test
+    public void testGetSmallestMultiple() {
+        final Problem5 problem5 = new Problem5();
+        Assertions.assertEquals(2520, problem5.getSmallestMultiple(10));
+    }
+
+}
