f6c0faff84b18c26227dcacdb437b8d7424bbc32
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-02-21 21:50:15 +0100

    Solved Problem 8

---
+++ src/test/java/engineer/thomas_werner/euler/Problem8Test.java
@@ -0,0 +1,30 @@
+package engineer.thomas_werner.euler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Problem8Test {
+
+    private Problem8 problem8;
+
+    @BeforeEach
+    public void setUp() {
+        problem8 = new Problem8();
+    }
+
+    @Test
+    public void testPrepareInput() {
+        final int[] data = problem8.prepareInput();
+        Assertions.assertEquals(1000, data.length);
+        Assertions.assertEquals(7, data[0]);
+        Assertions.assertEquals(3, data[1]);
+        Assertions.assertEquals(5, data[998]);
+        Assertions.assertEquals(0, data[999]);
+    }
+
+    @Test
+    public void testFindGreatestProductOfAdjecentDigits() {
+        Assertions.assertEquals(5832, problem8.findGreatestProductOfAdjecentDigits(4));
+    }
+}
