fc52a41b8b4ce959d30f237ebeb8c821d1e381ff
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-02-23 19:29:43 +0100

    Solved Problem 11

---
+++ src/test/java/engineer/thomas_werner/euler/Problem11Test.java
@@ -0,0 +1,16 @@
+package engineer.thomas_werner.euler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Problem11Test {
+
+    @Test
+    public void testGrid() {
+        final Problem11.Grid grid = new Problem11.Grid();
+        Assertions.assertEquals(8, grid.get(0,0));
+        Assertions.assertEquals(15, grid.get(0,5));
+        Assertions.assertEquals(48, grid.get(19,19));
+    }
+
+}
