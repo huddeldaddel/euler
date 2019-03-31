7d086c33ba9dfad3e5bd4f46fb25c0e868ede11d
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-03-31 22:01:39 +0200

    Implemented solver for problem 15

---
+++ src/main/kotlin/engineer/thomas_werner/euler/Problem15.kt
@@ -0,0 +1,35 @@
+package engineer.thomas_werner.euler
+
+fun main(args: Array<String>) {
+    val problem15 = Problem15(20)
+    println(problem15.countRoutes())
+}
+
+data class Problem15(val size: Int) {
+
+    private var count: Long = 0;
+
+    inline fun canGoDown(y: Int): Boolean {
+        return y < size;
+    }
+
+    inline fun canGoRight(x: Int): Boolean {
+        return x < size;
+    }
+
+    fun countRoutes() : Long {
+        count = 0
+        traverse(0, 0)
+        return count
+    }
+
+    fun traverse(x: Int, y: Int) {
+        if(canGoDown(y))
+            traverse(x, y +1)
+        if(canGoRight(x))
+            traverse(x +1, y)
+        if(!(canGoRight(x) || canGoDown(y)))
+            count++
+    }
+
+}
