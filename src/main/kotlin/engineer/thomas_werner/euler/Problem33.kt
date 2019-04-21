72287553fab9680e4cec715d1fbcb5df5737e506
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-04-21 11:13:32 +0200

    Refactored solver for problem 33

--- src/main/kotlin/engineer/thomas_werner/euler/Problem33.kt
+++ src/main/kotlin/engineer/thomas_werner/euler/Problem33.kt
@@ -2,23 +2,22 @@ package engineer.thomas_werner.euler
 
 fun main() {
     for(denominator in 11..99)
-        for(numerator in 10 until denominator) {
-            val value = numerator.toDouble() / denominator.toDouble()
-            numerator.toString().forEach {
-                if((it != '0') && denominator.toString().contains(it)) {
-                    val simplifiedNumerator = numerator.toString().replaceFirst(it.toString(), "").toInt()
-                    val simplifiedDenominator = denominator.toString().replaceFirst(it.toString(), "").toInt()
-                    val compValue = simplifiedNumerator.toDouble() / simplifiedDenominator.toDouble()
-                    if(Math.abs(value - compValue) < 0.000000001) {
-                        println("$numerator / $denominator = $simplifiedNumerator / $simplifiedDenominator")
-                    }
-                }
+        for(numerator in 10 until denominator)
+            checkFraction(denominator, numerator)
+
+    // This just prints the four fractions. I did the rest of the calculation on paper.
+}
+
+fun checkFraction(denominator: Int, numerator: Int) {
+    val value = numerator.toDouble() / denominator.toDouble()
+    numerator.toString().forEach {
+        if((it != '0') && denominator.toString().contains(it)) {
+            val simplifiedNumerator = numerator.toString().replaceFirst(it.toString(), "").toInt()
+            val simplifiedDenominator = denominator.toString().replaceFirst(it.toString(), "").toInt()
+            val compValue = simplifiedNumerator.toDouble() / simplifiedDenominator.toDouble()
+            if(Math.abs(value - compValue) < 0.000000001) {
+                println("$numerator / $denominator = $simplifiedNumerator / $simplifiedDenominator")
             }
         }
-
-    // I did the rest of the calculation on paper.
-    // Here is how to do it:
-    // 
-    // 1: find the multiply the identified numbers
-    // 2: simplify the result
+    }
 }
