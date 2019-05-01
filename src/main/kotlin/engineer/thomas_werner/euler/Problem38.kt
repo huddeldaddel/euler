6a03294bdfed6b4966551063673f00bc75bc4a17
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-05-01 21:23:34 +0200

    Refactored the code for problem 38

--- src/main/kotlin/engineer/thomas_werner/euler/Problem38.kt
+++ src/main/kotlin/engineer/thomas_werner/euler/Problem38.kt
@@ -9,23 +9,31 @@ class Problem38(private var biggestPandigital: Long = 0L) {
 
     fun findBiggestPandigital(): Long {
         var integer = 1
-        while(true) {
-            var concatenation = ""
-            var n = 1
-            while(concatenation.length < 9) {
-                concatenation += (integer * n++).toString()
-                if(9 == concatenation.length) {
-                    val longVal = concatenation.toLong()
-                    if(listOf(longVal).isPandigital(9) && longVal > biggestPandigital) {
-                        biggestPandigital = longVal
-                    }
-                }
-
-                if(3 == n && concatenation.length > 9) {
-                    return biggestPandigital
-                }
-            }
+        while(!isBiggestPandigitalFound(integer)) {
             integer++
+        }
+        return biggestPandigital
+    }
+
+    private fun isBiggestPandigitalFound(integer: Int): Boolean {
+        var concatenation = ""
+        var n = 1
+        while (concatenation.length < 9) {
+            concatenation += (integer * n++).toString()
+            updateResultIfPandigital(concatenation)
+            if (3 == n && concatenation.length > 9) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun updateResultIfPandigital(concatenation: String) {
+        if (9 == concatenation.length) {
+            val longVal = concatenation.toLong()
+            if (listOf(longVal).isPandigital(9) && longVal > biggestPandigital) {
+                biggestPandigital = longVal
+            }
         }
     }
 
