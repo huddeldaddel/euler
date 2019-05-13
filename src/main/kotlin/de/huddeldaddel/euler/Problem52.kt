335b8d6d34a54d27de8086c41a3a931da7bd9ba0
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-05-13 20:11:35 +0200

    Restructured code for problem 52

--- src/main/kotlin/de/huddeldaddel/euler/Problem52.kt
+++ src/main/kotlin/de/huddeldaddel/euler/Problem52.kt
@@ -8,11 +8,7 @@ fun main() {
     var num = base
     while(true) {
         while(num.toString().startsWith("1")) {
-            if(num.isPermutationOf(num * 2) &&
-                    num.isPermutationOf(num * 3) &&
-                    num.isPermutationOf(num * 4) &&
-                    num.isPermutationOf(num * 5) &&
-                    num.isPermutationOf(num * 6)) {
+            if(isMatch(num)) {
                 println(num)
                 exitProcess(0)
             }
@@ -21,5 +17,12 @@ fun main() {
         base *= 10
         num = base
     }
+}
 
+fun isMatch(num: Long): Boolean {
+    return num.isPermutationOf(num * 2) &&
+           num.isPermutationOf(num * 3) &&
+           num.isPermutationOf(num * 4) &&
+           num.isPermutationOf(num * 5) &&
+           num.isPermutationOf(num * 6)
 }
