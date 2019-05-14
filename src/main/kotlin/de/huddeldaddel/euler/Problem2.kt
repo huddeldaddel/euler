5aa611343471238eb1a945337daf6545b282fc32
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-05-14 07:25:29 +0200

    Converted solver for 2nd problem to Kotlin

---
+++ src/main/kotlin/de/huddeldaddel/euler/Problem2.kt
@@ -0,0 +1,20 @@
+package de.huddeldaddel.euler
+
+import de.huddeldaddel.euler.math.FibonacciSequence
+
+/**
+ * Solution for https://projecteuler.net/problem=2
+ */
+fun main() {
+    val fibonacciSequence = FibonacciSequence()
+    var f = fibonacciSequence.nextValue().toLong()
+    var result: Long = 0
+    while (f < 4_000_000) {
+        if (0L == f % 2) {
+            result += f
+        }
+        f = fibonacciSequence.nextValue().toLong()
+    }
+
+    println(result)
+}
